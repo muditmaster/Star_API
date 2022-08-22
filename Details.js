18853017838b1aaec01da5fb214fbf4a61608630
muditmaster <88135889+muditmaster@users.noreply.github.com>
2022-08-22 22:48:21 +0530

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import { Card, Icon } from "react-native-elements";
+import axios from "axios";
+export default class DetailsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      details: {},
+      imagePath: "",
+      url: `http://localhost:5000/star?name=${this.props.navigation.getParam(
+        "star_name"
+      )}`
+    };
+  }
+
+  componentDidMount() {
+    this.getDetails();
+  }
+  getDetails = () => {
+    const { url } = this.state;
+    axios
+      .get(url)
+      .then(response => {
+        this.setDetails(response.data.data);
+      })
+      .catch(error => {
+        Alert.alert(error.message);
+      });
+  };
+
+  render() {
+    const { details, imagePath } = this.state;
+    if (details.specifications) {
+      return (
+        <View style={styles.container}>
+          <Card
+            title={details.name}
+            image={imagePath}
+            imageProps={{ resizeMode: "contain", width: "100%" }}
+          >
+            <View>
+              <Text
+                style={styles.cardItem}
+              >{`Distance from Earth : ${details.distance_from_earth}`}</Text>
+              <Text
+                style={styles.cardItem}
+              >{`Distance from Sun : ${details.distance_from_their_sun}`}</Text>
+              <Text
+                style={styles.cardItem}
+              >{`Gravity : ${details.gravity}`}</Text>
+              <Text
+                style={styles.cardItem}
+              >{`Orbital Period : ${details.orbital_period}`}</Text>
+              <Text
+                style={styles.cardItem}
+              >{`Orbital Speed : ${details.orbital_speed}`}</Text>
+              <Text
+                style={styles.cardItem}
+              >{`Mass : ${details.mass}`}</Text>
+              <Text
+                style={styles.cardItem}
+              >{`Radius : ${details.radius}`}</Text>
+            </View>
+            <View style={[styles.cardItem, { flexDirection: "column" }]}>
+              <Text>{details.specifications ? `Specifications : ` : ""}</Text>
+              {details.specifications.map((item, index) => (
+                <Text key={index.toString()} style={{ marginLeft: 50 }}>
+                  {item}
+                </Text>
+              ))}
+            </View>
+          </Card>
+        </View>
+      );
+    }
+    return null;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  cardItem: {
+    marginBottom: 10
+  }
+});
